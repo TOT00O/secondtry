7631833cfd4012f851c5d96cd49e82ec92a8f5d3
Sabah Oyewole <saboye@elevad.umea.se>
2025-05-07 14:15:38 +0200

    log

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import '../app.postcss';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import { base } from '$app/paths';
+
+</script>
+
+<nav>
+	<a href="{base}/">Home</a>
+	<a href="{base}/devblog">Devblog</a>
+	<a href="{base}/projects">Projects</a>
+	<a href="{base}/#about">About</a>
+	<a href="{base}/contact">Contact</a>
+</nav>
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+    import 'animate.css';
+    import { base } from '$app/paths'; 
+
+</script>
+
+<main>
+    <div class="first">
+        <div class="head-container">
+        <h1 class="head-text"> Portfolio</h1>
+        </div>
+
+    </div>
+    <div class="about" id="about">
+        <h1>About</h1>
+        <p>Welcome to my portfolio! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and get to know me better.</p>
+        
+        
+    </div>
+</main>
+
+<style>
+
+    .first{
+        width: 100vw;
+        height: 100vh;
+        background:url(/lessbald.png) no-repeat ;
+        background-size: cover;
+    }
+
+    .about {
+        width: 100vw;
+        height: 100vh;
+        background-color: black;
+
+    }
+
+.head-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20vh;
+    width: 40vw;
+    top: 30%;
+    position: relative;
+    background: #ffffff;
+    font-family: 'TFMixVF', sans-serif;
+    overflow: hidden;
+}
+
+
+
+.head-text {
+    font-size: 8rem;
+    padding: 50px;
+    font-weight: bold;
+    color: transparent;
+    background-image: url('https://i.pinimg.com/originals/81/f6/5b/81f65bff8563907a8a673c6306fd83a2.gif'); 
+    background-size: 100% ;
+    background-position: 0 50%;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+}
+
+@font-face {
+  font-family: 'TFMixVF';
+  src: url(JangerRegular-lgYWq.otf);
+}
+
+</style>
