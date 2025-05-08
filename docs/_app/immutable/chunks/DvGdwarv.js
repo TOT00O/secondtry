bd9210cfc48af57eefe8c9addc8ba65622997fb0
Sabah Oyewole <saboye@elevad.umea.se>
2025-05-08 12:48:11 +0200

    message

---
+++ docs/_app/immutable/chunks/DvGdwarv.js
@@ -0,0 +1,7 @@
+import{s as g,n as r}from"./qK2_OJlR.js";import{S as k,i as S,d as a,a as n,c as d,g as _,b as h,e as v,f as p,s as w,t as y}from"./7l3ODnEi.js";function q(C){let e,x="Salutations",o,l,b="A couple of days ago i made the smart choice of deleting everything and restarting! So now i have a new and fresh start. do i hate myself for it?",i,m,c,f,u;return{c(){e=p("h1"),e.textContent=x,o=w(),l=p("p"),l.textContent=b,i=w(),m=p("br"),c=y(`
+...
+`),f=p("br"),u=y(`
+yes, but whats done is done... so no going back now! wish me luck please.`)},l(t){e=d(t,"H1",{"data-svelte-h":!0}),_(e)!=="svelte-xu0yc3"&&(e.textContent=x),o=h(t),l=d(t,"P",{"data-svelte-h":!0}),_(l)!=="svelte-fwqyoy"&&(l.textContent=b),i=h(t),m=d(t,"BR",{}),c=v(t,`
+...
+`),f=d(t,"BR",{}),u=v(t,`
+yes, but whats done is done... so no going back now! wish me luck please.`)},m(t,s){n(t,e,s),n(t,o,s),n(t,l,s),n(t,i,s),n(t,m,s),n(t,c,s),n(t,f,s),n(t,u,s)},p:r,i:r,o:r,d(t){t&&(a(e),a(o),a(l),a(i),a(m),a(c),a(f),a(u))}}}const B={title:"Devblog two",date:"2025-05-07"},{title:A,date:H}=B;class P extends k{constructor(e){super(),S(this,e,null,q,g,{})}}export{P as default,B as metadata};
